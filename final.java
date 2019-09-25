93220156f9cbb01f8177f2ada3325699cb857b70
hridayaAnnuncio <hridaya24x7@gmail.com>
2019-09-25 12:06:41 +0530

    final integration done!

--- final.java
+++ final.java
@@ -4,9 +4,11 @@ class final{
 		plus ad = new plus();
 		xyz  mul = new xyz();
 		xyz1 di =  new xyz1();
+		sub s1 = new sub();
 		a = ad.add(2,4);
 		b = mul.Mult(2,4);
 		c = di.div(4,2);
+		d= s1.s(4,2);
 		print(a,b,c);
 	}
 }
